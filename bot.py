c0b683ed24c79f5e39ab424c4b0f5d2199a6ec91
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-04-04 11:05:27 +0800

    baidu

--- bot.py
+++ bot.py
@@ -1,34 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import goslate as GS
 import sys
 import threading
 from wxbot import *
-import urllib2
-
+import httplib
+import md5
+import urllib
+import random
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import re
 
+class Tran:
+    def __init__(self):
+        self.appid = '20180404000142953'
+        self.secretKey = 'Ti2kr9gXh6xvY0yG1F4c'
+        self.httpClient = None
+        self.myurl = '/api/trans/vip/translate'
+        self.salt = random.randint(32768, 65536)
+
+    def translate(self,text,targetLang):
+        self.sign = self.appid+text+str(self.salt)+self.secretKey
+        m1 = md5.new()
+        m1.update(self.sign)
+        self.sign = m1.hexdigest()
+        self.myurl = self.myurl+'?appid='+self.appid+'&q='+urllib.quote(text)+'&from=auto'+'&to='+targetLang+'&salt='+str(self.salt)+'&sign='+self.sign
+ 
+        result = ''
+        try:
+            httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
+            httpClient.request('GET', myurl)
+ 
+            #response是HTTPResponse对象
+            response = httpClient.getresponse()
+            print response.read()
+        except Exception, e:
+            print e
+        finally:
+            if httpClient:
+            httpClient.close()
+
+    def transform(self,text):
+        return ''
 
 class wxGameBot(WXBot):
 
     def __init__(self):
         WXBot.__init__(self)
-        proxy_handler = urllib2.ProxyHandler({"http" : "http://proxy-domain.name:8080"})
-        proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
-                                    urllib2.HTTPSHandler(proxy_handler))
-        self.gs = GS.Goslate(opener=proxy_opener)
+        self.T = Tran()
         
     def transform(self, text):
-        m = self.gs.translate(text, 'en')
-        return self.gs.translate(m, 'zh')
+        return ''
 
     def handle_msg_all(self, msg):
         if msg['msg_type_id'] == 3 and msg['content']['type'] == 0:
             if msg['content']['detail'][0]['type'] == 'at' and msg['content']['detail'][0]['value'] == u'Bot':
-                self.send_msg_by_uid(self.transform(msg['content']['desc']), msg['user']['id'])
+                self.T.translate(msg['content']['desc'],'en')
+                #self.send_msg_by_uid(self.transform(msg['content']['desc']), msg['user']['id'])
 
 
 def main():
